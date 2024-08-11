f764bd985e1ec8d677254b5b869aa366003dcedf
Robbie West <robbie.west@ventriks.com>
2024-08-11 01:12:55 +0100

    Move around

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const windows = b.option(bool, "windows", "Target Microsoft Windows") orelse false;
-    const test_step = b.step("test", "Run unit tests");
 
     const exe = b.addExecutable(.{
         .name = "AssertionsBuild",
@@ -12,12 +11,5 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("./src/assertions.zig"),
-        .target = b.host,
-    });
-
     b.installArtifact(exe);
-    b.installArtifact(unit_tests);
-    test_step.dependOn(&unit_tests.step);
 }
