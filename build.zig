3d55953a84afd472e28c14355fab4ca1f66ed8f9
Robbie West <robbie.west@ventriks.com>
2024-08-11 01:35:09 +0100

    Static library build

--- build.zig
+++ build.zig
@@ -1,15 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const windows = b.option(bool, "windows", "Target Microsoft Windows") orelse false;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "AssertionsBuild",
-        .root_source_file = b.path("./src/assertions.zig"),
-        .target = b.resolveTargetQuery(.{
-            .os_tag = if (windows) .windows else null,
-        }),
+    const zigTestLib = b.addStaticLibrary(.{
+        .name = "zigTest",
+        .root_source_file = b.path("src/assertions.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    const demoExe = b.addExecutable(.{
+        .name = "demo",
+        .root_source_file = b.path("./src/demo.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(zigTestLib);
+    if (b.option(bool, "enable-demo", "install the demo too") orelse false)
+        b.installArtifact(demoExe);
 }
